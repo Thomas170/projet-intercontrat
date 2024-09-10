16f295f66b0a937b1a445edefe7e13c5a0309023
Mathis MOUTEL <mmoutel@consultants-solutec.fr>
2024-09-10 10:11:19 +0200

    add all

---
+++ frontend/src/app/components/cart/cart-products/cart-products.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-products',
+  standalone: true,
+  imports: [],
+  templateUrl: './cart-products.component.html',
+  styleUrl: './cart-products.component.css'
+})
+export class CartProductsComponent {
+
+}
